c80e81dbb804e3296239009909baaa54621bcf93
Talha-542 <ta662561@gmail.com>
2024-08-08 18:20:16 +0500

    beta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { deposit, withdraw, purchaseItem } from "./amountSlice";
 
 function App() {
+  const balance = useSelector((state) => state.balance.value);
+  const items = useSelector((state) => state.balance.items);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shop Items</h1>
+      <div>
+        {items.map((item) => (
+          <div key={item.name}>
+            <button onClick={() => dispatch(purchaseItem(item.name))}>
+              Buy {item.name} for ${item.cost}
+            </button>
+          </div>
+        ))}
+      </div>
+      <h2>Your Balance: ${balance}</h2>
+      <button onClick={() => dispatch(deposit(100))}>Deposit $100</button>
+      <button onClick={() => dispatch(withdraw(100))}>Withdraw $100</button>
     </div>
   );
 }
